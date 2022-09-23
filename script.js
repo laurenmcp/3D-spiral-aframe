050db3933441347162581fb6f311ea2aa72bf704
laurenmcp <lmcpeters3@gatech.edu>
2022-09-22 23:06:38 -0400

    trying to modify gltf model

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('start', {
         center.setAttribute('animation', {property: 'rotation', to: '0 360 0', easing: 'linear', repeat: 'indefinite', loop: 'true', dur: '15000'});
         for (let i = 0; i < 20; ++i) {
             var box = document.createElement('a-entity');
-            box.setAttribute('gltf-model', 'assets/trapezoid.gltf');
+            box.setAttribute('gltf-model', "assets/trapezoid.gltf");
             box.setAttribute('position', {x: (5 - i * .5), y: (10 - i), z: (0 - i)});
             box.setAttribute('color', '#E5A72E');
             box.setAttribute('animation', {property: 'position', from: {x: (5 - i * .5), y: (10 - i), z: (0 - i)}, to: {x: (5 - i * .5), y: (10 - i * 1.5), z: (0 - i)}, dur: 2000, dir: 'alternate', repeat: 'indefinite', loop: 'true'});
@@ -14,3 +14,15 @@ AFRAME.registerComponent('start', {
         document.getElementsByTagName('a-scene')[0].appendChild(center);
     }
 });
+
+/*experimenting with accessing gltf components*/
+AFRAME.registerComponent('gltf-model', {
+    init: function () {
+        this.el.addEventListener('model-loaded', () => {   
+          const block = this.el.getObject3D('mesh');
+          block.traverse(node => {
+              node.material.pbrMetallicRoughness.baseColorFactor.set('red');
+          });
+        });
+      }
+  });
